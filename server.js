e43660c10471fb1fb1a9b2c805438409ec3990f8
jalilakhtar <jalil@meditaz.com>
2017-07-15 11:20:54 -0700

    tables and reservation paths updated

--- server.js
+++ server.js
@@ -47,11 +47,11 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/add", function(req, res) {
-  res.sendFile(path.join(__dirname, "tables.html"));
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/add", function(req, res) {
+app.get("/reserve", function(req, res) {
   res.sendFile(path.join(__dirname, "reservation.html"));
 });
 
